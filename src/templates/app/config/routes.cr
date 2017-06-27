cd23f8584ed2940a0df6488863116eb30d786de5
Dru Jensen <drujensen@gmail.com>
2017-06-26 21:50:14 -0700

    match closer to amber routes

--- src/templates/app/config/routes.cr
+++ src/templates/app/config/routes.cr
@@ -2,25 +2,25 @@ require "../src/controllers/*"
 
 include Kemalyst::Handler
 
-get "/", home, index
+get "/", HomeController, :index
 
 # the `resources` macro will create these 7 routes for you:
-# get "/demos", demo, index
-# get "/demos/new", demo, new
-# post "/demos", demo, create
-# get "/demos/:id", demo, show
-# get "/demos/:id/edit", demo, edit
-# patch "/demos/:id", demo, patch
-# delete "/demos/:id", demo, delete
+# get "/demos", DemoController, :index
+# get "/demos/new", DemoController, :new
+# post "/demos", DemoController, :create
+# get "/demos/:id", DemoController, :show
+# get "/demos/:id/edit", DemoController, :edit
+# patch "/demos/:id", DemoController, :patch
+# delete "/demos/:id", DemoController, :delete
 
 # resources Demo
 
 # the `resource` macro will create the following routes for you
-# get "/demo/new", demo, new
-# post "/demo", demo, create
-# get "/demo", demo, show
-# get "/demo/edit", demo, edit
-# patch "/demo", demo, update
-# delete "/demo", demo, delete
+# get "/demo/new", DemoController, :new
+# post "/demo", DemoController, :create
+# get "/demo", DemoController, :show
+# get "/demo/edit", DemoController, :edit
+# patch "/demo", DemoController, :update
+# delete "/demo", DemoController, :delete
 
 # resource Demo
